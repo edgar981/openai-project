490446231bcac6552cdf0505ca196ad68923e83a
Edgar <davidnavarro81230@outlook.com>
2023-05-23 13:33:05 -0500

    Auth page and credentials

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
       <div>
-
+          <h1>Hola</h1>
+          <div>
+              <Link href={"/api/auth/login"}>Login</Link>
+          </div>
       </div>
   )
   ;
